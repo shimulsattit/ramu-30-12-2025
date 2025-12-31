92f2c6f1355e0de4cb6f849e4c1288d4d8e04009
shimulsattit <shimulsattit@gmail.com>
2026-01-01 01:47:56 +0600

    fix: Update location-map partial to use embed_code field

--- resources/views/partials/location-map.blade.php
+++ resources/views/partials/location-map.blade.php
@@ -11,11 +11,9 @@
             </h5>
         </div>
         <div class="card-body p-0">
-            @if($locationMap->embed_url)
-                <div class="ratio ratio-16x9" style="height: {{ $locationMap->height ?? '400' }}px;">
-                    <iframe src="{{ $locationMap->embed_url }}" style="border:0;" allowfullscreen="" loading="lazy"
-                        referrerpolicy="no-referrer-when-downgrade">
-                    </iframe>
+            @if($locationMap->embed_code)
+                <div class="ratio ratio-16x9" style="min-height: 400px;">
+                    {!! $locationMap->embed_code !!}
                 </div>
             @endif
         </div>
